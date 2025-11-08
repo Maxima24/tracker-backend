cb507727419d3e1350ae28e8f70443bc1de6744b
Steel Maxima <steelmaxima21@gmail.com>
2025-11-08 23:52:51 +0100

    hjd,,e,u

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+  origin: "*", // allow everything for now
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type, Authorization",
+});
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
